949cacf3c2b41ffc912348173adf2e4064d1fc16
Bukrek35TR <fatih.yigit.35@gmail.com>
2023-02-22 00:11:35 +0300

    DataBinding implemented

--- app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.java
@@ -1,46 +1,41 @@
 package com.example.mvvmkullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.mvvmkullanimi.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewSonuc;
-    private EditText editTextSayi1, editTextSayi2;
-    private Button buttonTopla, buttonCarp;
+    private ActivityMainBinding tasarim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tasarim = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        textViewSonuc = findViewById(R.id.textViewSonuc);
-        editTextSayi1 = findViewById(R.id.editTextSayi1);
-        editTextSayi2 = findViewById(R.id.editTextSayi2);
-        buttonTopla = findViewById(R.id.buttonTopla);
-        buttonCarp = findViewById(R.id.buttonCarp);
+        tasarim.textViewSonuc.setText("0");
 
-        textViewSonuc.setText("0");
-
-        buttonTopla.setOnClickListener(v -> {
-            int sayi1 = Integer.parseInt(editTextSayi1.getText().toString());
-            int sayi2 = Integer.parseInt(editTextSayi2.getText().toString());
+        tasarim.buttonTopla.setOnClickListener(v -> {
+            int sayi1 = Integer.parseInt(tasarim.editTextSayi1.getText().toString());
+            int sayi2 = Integer.parseInt(tasarim.editTextSayi2.getText().toString());
 
             int toplam = sayi1 + sayi2;
 
-            textViewSonuc.setText(String.valueOf(toplam));
+            tasarim.textViewSonuc.setText(String.valueOf(toplam));
 
         });
-        buttonCarp.setOnClickListener(v -> {
-            int sayi1 = Integer.parseInt(editTextSayi1.getText().toString());
-            int sayi2 = Integer.parseInt(editTextSayi2.getText().toString());
+        tasarim.buttonCarp.setOnClickListener(v -> {
+            int sayi1 = Integer.parseInt(tasarim.editTextSayi1.getText().toString());
+            int sayi2 = Integer.parseInt(tasarim.editTextSayi2.getText().toString());
 
             int carpim = sayi1 * sayi2;
 
-            textViewSonuc.setText(String.valueOf(carpim));
+            tasarim.textViewSonuc.setText(String.valueOf(carpim));
 
         });
 
